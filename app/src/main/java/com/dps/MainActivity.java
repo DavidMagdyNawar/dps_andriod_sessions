a6d293c8851b1a65c701e2762502393f7d2dee3d
DavidRiad <davidmagdynawar@gmail.com>
2020-10-19 16:51:05 +0200

    creating simple layout for greeting :sparkles:

--- app/src/main/java/com/dps/MainActivity.java
+++ app/src/main/java/com/dps/MainActivity.java
@@ -1,17 +1,14 @@
 package com.dps;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //cast our views
+        final TextView helloTextView = findViewById(R.id.hello);
+        final EditText nameEditText = findViewById(R.id.nameEditText);
+        Button clickMeButton = findViewById(R.id.clickMeButton);
+
+        //on click the button " click me "
+        clickMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // check if the edit text is empty
+                if(nameEditText.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "Please enter your name", Toast.LENGTH_SHORT).show();
+                }
+                // if it's not empty,
+                else{
+                    helloTextView.setText("Hello " + nameEditText.getText().toString()+"!");
+                }
+            }
+        });
 
     }
 }
